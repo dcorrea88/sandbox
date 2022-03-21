3e6e5017bd8d190459d37c6f82a8f77bb4c018e9
Daniel Correa <dcorrea2988@gmail.com>
2022-03-21 13:53:50 -0400

    Debugging Practice Fix code

--- 100days/print_functions.py
+++ 100days/print_functions.py
@@ -5,3 +5,12 @@ print("print('what to print')")
 print ("Hello World!\nHello World!\nHello World!")
 
 print("Hello"+" "+"World!")
+
+# Debugging Practice
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
