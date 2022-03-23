93c883070a87ab01f867dbfed59e29f5a0ed224e
Daniel Correa <dcorrea2988@gmail.com>
2022-03-23 14:10:01 -0400

    Variable Exercise

---
+++ 100days/variable.py
@@ -0,0 +1,13 @@
+name="Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+# Short code for 
+# 'print( len( input("What is your name? ") ) )'
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
